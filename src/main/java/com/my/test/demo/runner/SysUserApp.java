dcc8063630a96f64f8e7cebe98e9e99ab3f20242
党诗林 <545056314@qq.com>
2019-02-27 16:23:19 +0800

    添加过滤器

--- src/main/java/com/my/test/demo/runner/SysUserApp.java
+++ src/main/java/com/my/test/demo/runner/SysUserApp.java
@@ -1,8 +1,11 @@
 package com.my.test.demo.runner;
 
+import com.my.test.demo.filter.LoginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -16,5 +19,25 @@ import org.springframework.context.annotation.ComponentScan;
 public class SysUserApp {
     public static void main(String[] args) {
         SpringApplication.run(SysUserApp.class,args);
+    }
+
+    @Bean
+    public FilterRegistrationBean registerFilter() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+
+        String errUrl = "login.html";
+        registration.setFilter(loginFilter());
+        // 过滤应用程序中所有资源,当前应用程序根下的所有文件包括多级子目录下的所有文件，注意这里*前有“/”
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("ERR_URL", errUrl);
+        registration.setName("WebAccessAuthorizeFilterMvc");
+
+        return registration;
+    }
+
+    @Bean
+    public LoginFilter loginFilter(){
+        return new LoginFilter();
     }
 }
