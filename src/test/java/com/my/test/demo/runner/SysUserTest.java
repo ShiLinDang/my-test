0532eca3007b8315a00807df167546344dcb3225
党诗林 <545056314@qq.com>
2019-03-07 11:12:51 +0800

    单元测试

---
+++ src/test/java/com/my/test/demo/runner/SysUserTest.java
@@ -0,0 +1,32 @@
+package com.my.test.demo.runner;
+
+import com.my.test.demo.entity.SysUser;
+import com.my.test.demo.service.SysUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Description:
+ * @author:dangshilin
+ * @date 2019/3/710:34
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SysUserApp.class)
+public class SysUserTest{
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    @Test
+    public void getList(){
+        List<SysUser> userList = sysUserService.getUserList();
+        System.out.println("userList:" + userList.toString());
+    }
+}
