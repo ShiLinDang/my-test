2f1ab7608c98a579ab6cca29d3e9da62703dc9ce
党诗林 <545056314@qq.com>
2019-08-16 14:16:52 +0800

    测试用例

--- src/test/java/com/my/test/demo/runner/SysUserTest.java
+++ src/test/java/com/my/test/demo/runner/SysUserTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.List;
  * @author:dangshilin
  * @date 2019/3/710:34
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = SysUserApp.class)
 public class SysUserTest{
 
